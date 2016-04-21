73c0ff2fa931dc4ed46493e6168def5b33765f30
cedricCaffy <cedric.caffy@hotmail.fr>
2016-04-21 14:49:12 +0200

    SaisirVol

--- src/controllers/SaisirVolController.java
+++ src/controllers/SaisirVolController.java
@@ -1,19 +1,31 @@
 package controllers;
 
-import model.classes.avion.Avion;
-import model.classes.membres.Membre;
-import model.classes.vol.TypeVol;
-import model.dao.AvionBD;
-import application.MainApp;
+import util.Temps;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Pair;
 import javafx.util.StringConverter;
+import model.classes.avion.Avion;
+import model.classes.membres.Membre;
+import model.classes.vol.Aerodrome;
+import model.classes.vol.TypeVol;
+import model.classes.vol.Vol;
+import model.dao.AvionBD;
+import application.MainApp;
 
 public class SaisirVolController {
+	private ObservableList<Vol> listVols;
 	private Membre membre;
 	private AvionBD avionBD;
 	private MainApp mainApp;
@@ -27,10 +39,28 @@ public class SaisirVolController {
 	@FXML
 	private ComboBox<TypeVol> cb_typeVol;
 	
+	@FXML
+	private Button b_ajouterEtape;
+	
+	@FXML
+	private TableView<Vol> tv_etapes;
+	
+	@FXML
+	private TableColumn<Vol,String> colonne_aerodromeDepart;
+	
+	@FXML
+	private TableColumn<Vol,String> colonne_aerodromeArrivee;
+	
+	@FXML
+	private TableColumn<Vol,String> colonne_tempsVol;
+	
 	public SaisirVolController(){}
 	
 	@FXML
 	private void initialize(){
+		this.listVols=FXCollections.observableArrayList();
+		tv_etapes.setEditable(true);
+		/*colonne_aerodromeDepart.setCellFactory(TextFieldTableCell.<Vol>forTableColumn());*/
 	}
 	
 	/**
@@ -47,6 +77,20 @@ public class SaisirVolController {
 	}
 	
 	/**
+	 * Action qui suit le click sur le bouton Ajouter Etape
+	 */
+	@FXML
+	private void actionAjouterEtape(){
+		int tailleTableVols=tv_etapes.getItems().size();
+		if(tailleTableVols==0){
+			this.listVols.add(new Vol(null,new Temps(0,0),new Aerodrome("",""),new Aerodrome("",""),null,0));
+		}else{
+			listVols.add(new Vol(null,tv_etapes.getItems().get(tailleTableVols-1).getTempsVol(),tv_etapes.getItems().get(tailleTableVols-1).getAerodromeDepart(),tv_etapes.getItems().get(tailleTableVols-1).getAerodromeArrivee(),null,0));
+		}
+		tv_etapes.setItems(listVols);
+	}
+	
+	/**
 	 * Action qui suit le clic sur le bouton annuler
 	 */
 	@FXML
@@ -54,6 +98,32 @@ public class SaisirVolController {
 		mainApp.afficherEcranAccueil(this.membre);
 	}
 	
+	private void initialiserTableVols(){
+		colonne_aerodromeDepart.setCellValueFactory((cellData)->cellData.getValue().getAerodromeDepart().getIdentifiantProperty());
+		colonne_aerodromeDepart.setCellFactory(TextFieldTableCell.<Vol>forTableColumn());
+		colonne_aerodromeDepart.setOnEditCommit(
+	            (CellEditEvent<Vol, String> t) -> {
+	                ((Vol) t.getTableView().getItems().get(
+	                        t.getTablePosition().getRow())
+	                        ).setAerodromeDepart(new Aerodrome("",t.getNewValue()));
+	        });
+		colonne_aerodromeArrivee.setCellValueFactory((cellData)->cellData.getValue().getAerodromeArrivee().getIdentifiantProperty());
+		colonne_aerodromeArrivee.setCellFactory(TextFieldTableCell.<Vol>forTableColumn());
+		colonne_aerodromeArrivee.setOnEditCommit(
+	            (CellEditEvent<Vol, String> t) -> {
+	                ((Vol) t.getTableView().getItems().get(
+	                        t.getTablePosition().getRow())
+	                        ).setAerodromeArrivee(new Aerodrome("",t.getNewValue()));
+	        });
+		colonne_tempsVol.setCellValueFactory((cellData)->cellData.getValue().getTempsVol().toStringProperty());
+		colonne_tempsVol.setCellFactory(TextFieldTableCell.<Vol>forTableColumn());
+		colonne_tempsVol.setOnEditCommit(
+	            (CellEditEvent<Vol, String> t) -> {
+	                ((Vol) t.getTableView().getItems().get(
+	                        t.getTablePosition().getRow())
+	                        ).setTempsVol(new Temps(0,Integer.parseInt(t.getNewValue())));
+	        });
+	}
 	/**
 	 * Methode qui gere les actions apres
 	 * le chargement de la page
@@ -62,6 +132,7 @@ public class SaisirVolController {
 		avionBD=new AvionBD();
 		ajouterAvionsComboBox();
 		ajouterTypeVolComboBox();
+		initialiserTableVols();
 	}
 	
 	/**
