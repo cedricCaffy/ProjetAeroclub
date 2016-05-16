a9ac9239f886914e8dbaa79d2ff79933ff64adc3
cedricCaffy <cedric.caffy@hotmail.fr>
2016-05-16 18:37:57 +0200

    PaiementDAO

--- Analyse/BDD/Jeux essais.sql
+++ Analyse/BDD/Jeux essais.sql
@@ -2,11 +2,13 @@ INSERT INTO AEROCLUB (idaeroclub,nom) VALUES (1,'Aéroclub Périgueux Bassillac'
 INSERT INTO ADRESSE (rue,ville,codepostal,numero)
 VALUES
 ('Chez Michelet','Mauzens et Miremont',24260,NULL),
-('rue de maxime','Saint-Flour',15100,3);
+('rue de maxime','Saint-Flour',15100,3),
+('rue des instructeurs','Sarlat',24260,NULL);
 
 INSERT INTO MEMBRE (nom,prenom,idadr,email,numtel,datenaissance,solde,idaeroclub,login,mdp) VALUES
 ('CAFFY','Cédric',1,'cedric.caffy@hotmail.fr','0553058803','1993-05-24',50,1,'cedric','cedric'),
-('AUZANNEAU','Maxime',2,'maauzannea@poste.isima.fr','0102030405','1994-12-31',-5,1,'maxime','maxime');
+('AUZANNEAU','Maxime',2,'maauzannea@poste.isima.fr','0102030405','1994-12-31',-5,1,'maxime','maxime'),
+('DELPECH','Jean-Michel',3,'jm_delpech@hotmail.fr','0102030405','1980-10-05',0,1,'DPH','DPH');
 
 INSERT INTO AVION (nomavion,typeavion,immatriculation,consommation,capacitereservoir,nbplace,massemaximale,couthoraire,disponibilite,vitessecroisiere,idaeroclub)
 VALUES
@@ -17,18 +19,24 @@ VALUES
 INSERT INTO DROITS (idmembre,instructeur,administrateur,mecanicien,pilote)
 VALUES
 (1,'INSTRUCTEUR','ADMIN','MECANICIEN','PILOTE'),
-(2,NULL,NULL,NULL,'PILOTE');
+(2,NULL,NULL,NULL,'PILOTE'),
+(3,'INSTRUCTEUR',NULL,NULL,'PILOTE');
 
 INSERT INTO PILOTE(idmembre,datevaliditevisitemedicale)
 VALUES
 (1,'2016-08-09'),
-(2,'2016-12-24');
+(2,'2016-12-24'),
+(3,'2017-10-21');
 
 INSERT INTO BREVET (idpilote,nombrevet,datevalidite,dateObtention)
 VALUES
 (1,'PPL-A','2016-05-24','2013-05-24'),
 (2,'BB','2016-08-08','2013-08-08'),
-(1,'CPL-A','2016-06-25','1993-06-25');
+(1,'CPL-A','2016-06-25','1993-06-25'),
+(3,'PPL-A','2017-10-21','2000-05-24'),
+(3,'CPL-A','2017-12-24','2005-04-21');
 
 INSERT INTO INSTRUCTEUR (numeroinstructeur,couthoraire,idpilote)
-VALUES('FI-000001',20,1);
+VALUES
+('FI-000001',20,1),
+('FI-000002',30,3);
---
+++ src/model/dao/PaiementDAO.java
@@ -0,0 +1,7 @@
+package model.dao;
+
+import model.classes.paiement.Paiement;
+
+public interface PaiementDAO {
+	void insererPaiement(Paiement paiement);
+}
