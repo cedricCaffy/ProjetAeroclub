fe8ccae77331b98ee58e697daadda116eaf741fd
cedricCaffy <cedric.caffy@hotmail.fr>
2016-05-21 13:44:03 +0200

    Rapport bien avancé :+1:

--- src/model/dao/AeroclubDAOImpl.java
+++ src/model/dao/AeroclubDAOImpl.java
@@ -25,20 +25,24 @@ public class AeroclubDAOImpl implements AeroclubDAO{
 	 */
 	@Override
 	public String getNomAeroclub() throws DAOException{
-		Connection connexion=null;
-		PreparedStatement preparedStatement=null;
-		ResultSet resultSet=null;
-		String nomAeroclub=null;
+		Connection connexion=null;//Contient la connexion a la BDD
+		PreparedStatement preparedStatement=null; //contiendra la requete preparee SQL
+		ResultSet resultSet=null; //Contient le resultat de la requete
+		String nomAeroclub=null;  // Contient le nom de l'aeroclub
 		try{
-			connexion=this.connexion.getConnexion();
+			connexion=this.connexion.getConnexion();//Connexion a la BDD
+			//Initialisation de la requete preparee
 			preparedStatement=DAOUtilitaire.initialiserRequetePreparee(connexion,AeroclubDAOImpl.requeteGetNomAeroclub,true,(Object)null);
+			//Execution de la requete
 			resultSet=preparedStatement.executeQuery();
 			if(resultSet.next()){
+				//Resultat trouve
 				nomAeroclub=resultSet.getString("nom");
 			}
 		}catch(SQLException e){
 			throw new DAOException(e);
 		}finally{
+			//Fermeture propre de la connexion, du resultSet et du preparedStatement
 			DAOUtilitaire.fermeturesSilencieuses(resultSet,preparedStatement,connexion);
 		}
 		return nomAeroclub;
